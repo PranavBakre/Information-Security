61c38a14d7893d13dd8ffcf5de3841a78a7a58dc
pranav bakre <psbakre@yahoo.com>
2019-11-04 15:31:10 +0530

    Add project files.

---
+++ Caesar Cipher/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Caesar_Cipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the string");
+            var Message = Console.ReadLine();
+            var EncryptionString=Encrypt(Message);
+            Console.WriteLine($"Encrypted Message={EncryptionString}");
+            Console.WriteLine("Decrypted Message={0}",Decrypt(EncryptionString));
+        }
+
+        public static string Encrypt(string Message)
+        {
+
+                
+                char[] MessageArray, EncryptedMessage;
+                MessageArray = Message.ToCharArray();
+                EncryptedMessage = new char[MessageArray.Length];
+
+                for (int i = 0; i < MessageArray.Length; i++)
+                {
+                    if (MessageArray[i] >= 'x' && MessageArray[i] <= 'z')
+                    {
+                        EncryptedMessage[i] = (char)(MessageArray[i] % 120 + 97);
+                    }
+                    else if (MessageArray[i] >= 'X' && MessageArray[i] <= 'Z')
+                    {
+                        EncryptedMessage[i] = (char)(MessageArray[i] % 90 + 65);
+                    }
+                    else
+                    {
+                        EncryptedMessage[i] = (char)(MessageArray[i] + 3);
+                    }
+                }
+
+
+                return (new string(EncryptedMessage));
+
+            }
+
+        public static string Decrypt(string EncryptedMessage)
+        {
+            var EncryptedArray = EncryptedMessage.ToCharArray();
+            char[] DecryptionArray=new char[EncryptedArray.Length];
+            for (int i= 0; i< EncryptedArray.Length;i++ ) {
+                if ((EncryptedArray[i]<=67 && EncryptedArray[i]>=65) || (EncryptedArray[i] <= 99 && EncryptedArray[i] >= 97))
+                {
+                    DecryptionArray[i] = (char)(EncryptedArray[i] +26 - 3);
+                }
+                
+                else
+                {
+                    DecryptionArray[i] = (char)(EncryptedArray[i] - 3);
+                }
+            }
+            return (new string(DecryptionArray));
+        }
+        
+    }
+}
---
+++ MonoAlphabetic Cypher/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+namespace MonoAlphabetic_Cypher
+{
+    class Program
+    {
+        static Dictionary<char,char> D;
+        static void Main(string[] args)
+        {
+            D = new Dictionary<char, char>();
+            int n = 122;
+            for (char i=(char)97,j=(char)n;i<=n;i++,j--)
+            {
+                
+                //var d = Convert.ToChar(Console.ReadLine());
+                D.Add(i, j/*d*/);
+            }
+            n = 90;
+            for (char i = (char)65, j = (char)n; i <= n; i++, j--)
+            {
+
+                //var d = Convert.ToChar(Console.ReadLine());
+                D.Add(i, j/*d*/);
+            }
+            D.Add(' ', ' ');
+
+            Console.WriteLine("Enter the string");
+            var Message = Console.ReadLine();
+            var EncryptedMessage = Encrypt(Message);
+            Console.WriteLine($"\n\n\nEncrypted Message: {EncryptedMessage}");
+            var DecryptedMessage = Decrypt(EncryptedMessage);
+
+            Console.WriteLine($"Decrypted Message: { DecryptedMessage}");
+
+
+        }
+
+        public static string Encrypt(string Message)
+        {
+            var MessageArray = Message.ToCharArray();
+            char[] EncryptionString = new char[MessageArray.Length];
+            for (int i = 0; i < MessageArray.Length; i++)
+            {
+                EncryptionString[i] = D[MessageArray[i]];
+            }
+            return new string(EncryptionString);
+        }
+
+        public static string Decrypt(string EncryptedMessage)
+        {
+            var EncryptedArray = EncryptedMessage.ToCharArray();
+            char[] DecryptedArray = new char[EncryptedArray.Length];
+            for (int i=0;i<EncryptedArray.Length;i++)
+            {
+
+                foreach(KeyValuePair<char,char> K in D)
+                {
+                    if (K.Value == EncryptedArray[i])
+                    {
+                        DecryptedArray[i] = K.Key;
+                    }
+                }
+
+            }
+
+            
+            return new string(DecryptedArray);
+        }
+    }
+}
