d82a2479b6eebb0a1c435caf97e9a0463db9d9c7
pranav bakre <psbakre@yahoo.com>
2019-11-22 13:50:44 +0530

    SHA-1

---
+++ SHA-1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SHA_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SHA1 sha = new SHA1CryptoServiceProvider();
+            try
+            {
+                FileStream file = new FileStream("Program.cs",FileMode.Open);
+                byte[] ByteFile=new byte[file.Length];
+                int BytesRead = file.Read(ByteFile);
+                byte[] result = sha.ComputeHash(ByteFile);
+                FileStream fout= new FileStream("ProgHash.txt",FileMode.Create);
+                foreach (char ch in ByteFile)
+                    Console.Write(ch);
+                foreach (char ch in result)
+                    Console.Write(ch);
+                fout.Write(result);
+                file.Close();
+                fout.Close();
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
